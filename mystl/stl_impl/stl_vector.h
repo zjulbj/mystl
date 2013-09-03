f75830f2fb4a4bc83828078f12f1a3c256e71287
zjulbj <numbcode@gmail.com>
2013-09-03 13:31:08 +0800

    vector
    assign,insert

--- mystl/stl_impl/stl_vector.h
+++ mystl/stl_impl/stl_vector.h
@@ -11,6 +11,7 @@
  */
 #ifndef _MSTL_VECTOR_H
 #define _MSTL_VECTOR_H
+#include<type_traits>
 namespace numb
 {
 	//a base class to allocate/deallocate memory,without initializing. 
@@ -105,7 +106,7 @@ namespace numb
 	 //nested typedefs
 		//internal typedefs
 		typedef	 typename Alloc::value_type 	Alloc_value_type;
-		typedef Vec_base<Tp,Alloc>			Base;
+		typedef  Vec_base<Tp,Alloc>			    Base;
 		typedef	 typename Base::Tp_alloc_type	 Tp_alloc_type;
 		/*
 		 *To-do:Concept requirements
@@ -124,7 +125,7 @@ namespace numb
 			typedef  Alloc 								allocator_type;
 			typedef typename numb::_normal_iterator<pointer,vector> iterator;
 			typedef typename numb::_normal_iterator<const_pointer,vector> 
-				const_iterator;
+													const_iterator;
 			/*To do:
 			 *	1.reverse_iterator
 			 *	2.const_reverse_iterator	
@@ -136,35 +137,55 @@ namespace numb
 			using Base::impl;
 		public:
 	
-	//	Construct/copy/destory	
-		/*	default ctor
+	//Construct/copy/destory	
+		/*	
+		 *  default ctor
 		 *	creates no element;
 		 */
 		vector():Base(){}
-		/*ctor with a @param a An allocator
-		 *creates no element
+		/* 
+		 * ctor with a @param a An allocator
+		 * creates no element
 		 */
 		explicit vector(const allocator_type& a):Base(a){}
-				
-		/**
-        *  Creates a vector with n copies of value
-        *  @param  n  The number of elements to initially create.
-        *  @param  value  An element to copy.
-        *  @param  a  An allocator.
-        */
+		/* 
+		 * ctor with a @param n Number of elements
+		 * creates n default constructed elemets 
+		 */	
+		 explicit vector(size_type n):Base(n)
+		 {	M_default_initialize(n);}
+		/*
+         *  Creates a vector with n copies of value
+         *  @param  n  The number of elements to initially create.
+         *  @param  value  An element to copy.
+         *  @param  a  An allocator.
+         */
 		explicit
 		vector(size_type n, 
 				const value_type& value = value_type(),
 				const allocator_type& a = allocator_type())
 		: Base(n, a)
 		{ 
-			//to do:M_fill_initialize
 			M_fill_initialize(n,value); 
 		}
-		/**
-        *  Copy constructor.
-        *  @param  x  A vector of identical element and allocator types.
-        */
+		/*
+         *  Creates a vector from a range
+         *  @param  first  Input iterator.
+         *  @param  last   Input iterator.
+         *  @param  a  An allocator.
+         */
+		 template<typename InputIter>
+		 vector(InputIter first,InputIter last,const allocator_type a=allocator_type())
+		 :Base(a)
+		 {
+			//avoid ambiguity from vector(int,int)
+			typedef typename std::is_integral<InputIter>::type Integral;
+			M_fill_range(first,last,Integral());	
+		 }
+		/*
+         *  Copy constructor.
+         *  @param  x  A vector of identical element and allocator types.
+         */
 		vector(const vector& x)
 		: Base(x.size(), x.get_Tp_allocator())
         { 
@@ -180,12 +201,26 @@ namespace numb
 			std::_Destroy(this->impl.start, this->impl.finish,
 		      get_Tp_allocator()); 
 		}
-		
+		//Copy assignment operator.
+		//Replaces the contents with a copy of the contents of other.
 		vector&
-		operator=(const vector& __x);
-		//to do:assign
+		operator=(const vector& x);
+	//assign
+		/*
+		 * Replaces the contents with n copies of v
+		 * Unlike copy ctor,extra memory and allocator are not copied
+		 */
+		void assign(size_type n,const value_type& v)
+		{	M_fill_assign(n,v);	}
 		
-		//vector::get_allocator
+		template<class InputIter>
+		void assign(InputIter first,InputIter last)
+		{
+			//avoid ambiguity from assign(int,int);
+			typedef typename std::is_integral<InputIter>::type Integral;
+			M_assign_range(first,last,Integral());
+		}
+	//get_allocator
 		using Base::get_allocator;
 		
 	// Iterators:	
@@ -221,7 +256,7 @@ namespace numb
 		{ return begin() == end(); }
 		
 		//to do:reverse
-		//to do:shrink_to_fit
+		//to do:shrink_to_fit  c++11
 		
 	// Element access:	
 		//return n-th element's reference
@@ -260,7 +295,7 @@ namespace numb
 		{ return *(end()-1);}
 		const_reference back()const
 		{ return *(end()-1);}
-		//to do:data()
+		//to do:data() c++11
 		
 	// Modifiers
 		//Add a element at the end of vector
@@ -274,7 +309,7 @@ namespace numb
 			else
 			{
 				//to do
-				;
+				M_insert_aux(end(),x);
 			}
 		}
 		//Remove last element
@@ -283,23 +318,382 @@ namespace numb
 			--this->impl.finish;
 			this->impl.destroy(this->impl.finish);
 		}
-		//to do:insert
+		
+		//Insert
+		//insert(p,v)
+		iterator insert(iterator p, const value_type& v);
+		//insert(p,n,v)
+		void insert(iterator p, size_type n, const value_type& x)
+		{ M_fill_insert(p, n,x); }
+		//insert(p,first,last)
+		template<typename InputIter>
+        void insert(iterator p, InputIter first,InputIter last)
+        {
+			// Check whether it's an integral type.  If so, it's not an iterator.
+			typedef typename std::is_integral<InputIter>::type Integral;
+			M_insert_range(p, first, last, Integral());
+		}
 		//to do:erase
-		//to do:swap
-		//to do:clear
-		//to do:resize 
+		//erase(p);
+		iterator erase(iterator p);
+		//erase(first,last)
+		iterator erase(iterator first, iterator last);
+		//swap with vector x
+		void swap(vector& x)
+		{
+			std::swap(this->impl.start, x.impl.start);
+			std::swap(this->impl.finish, x.impl.finish);
+			std::swap(this->impl.end_of_storage,
+				x.impl.end_of_storage);
+			//to do:swap allocator
+		}
+		//clear all the element
+		void clear()
+		{	M_erase_at_end(this->impl.start);}
+		//resize the vector with new_size element v
+		void resize(size_type new_size,value_type v)
+		{
+			if(new_size>size())//insert before the end
+			{
+				insert(end(), new_size-size(), v);
+			}
+			else if(new_size<size())//erase the redundant
+			{
+				M_erase_at_end(this->impl.start + new_size);
+			}
+			//do nothing if new_size==size()
+		}
 	protected:
-		//internal functions
+	//internal functions
+		/*
+		 *  Memory expansion handler. To obtain @a n bytes of memory, 
+		 *  and then copies [first,last) into it.
+         */
+		template<class ForwardIter>
+		pointer 
+		M_allocate_and_copy(size_type n,
+							ForwardIter first,
+							ForwardIter last)
+		{
+			//allocate new memory
+			pointer tmp=M_allocate(n);
+			try
+			{
+				std::__uninitialized_copy_a(first,last,tmp,get_Tp_allocator());
+				return tmp;
+			}
+			catch(...)
+			{
+				//if failed,release the memory
+				M_deallocate(tmp,n);
+				throw;
+			}
+		}
+		/*
+		 * Internal constructor functions follow.
+		 * Called by the range constructor to implement
+		 */
+		//call by vector(n) ctor
+		void M_default_initialize(size_type n)
+		{
+			//create n default elements
+			std::__uninitialized_default_n_a(this->impl.start, n, 
+					 get_Tp_allocator());
+			this->impl.finish = this->impl.end_of_storage;
+		}
 		//call by vector(n,v,a) ctor
-		void
-		M_fill_initialize(size_type n, const value_type& value)
+		void M_fill_initialize(size_type n, const value_type& value)
 		{
 			std::__uninitialized_fill_n_a(this->impl.start, n,value, 
 				      get_Tp_allocator());
 			this->impl.finish = this->impl.end_of_storage;
 		}
+		//call by vector(first,last) ctor
+		//in case of vector(int,int)
+		template<class Integer>
+		void M_fill_range(Integer n,
+						  Integer v,
+						  std::true_type)
+		{
+			this->impl.start=M_allocate(static_cast<size_type>(n));
+			this->impl.end_of_storage=this->impl.start+static_cast<size_type>(n);
+			M_fill_initialize(static_cast<size_type>(n),v);
+		}
+		//construct from a range
+		template<class InputIter>
+		void M_fill_range(InputIter first,
+						  InputIter last,
+						  std::false_type)
+		{
+			//depend on iterator type
+			M_fill_range_aux(first,last,numb::_iterator_category(first));
+		}
+		//if it's input iterator
+		template<class InputIter>
+		void M_fill_range_aux(
+				InputIter first,
+				InputIter last,
+				numb::input_iterator_tag)
+		{
+			for(;first!=last;++first)
+				push_back(*first);
+		}
+		//or it's forward iterator
+		template<class ForwardIter>
+		void M_fill_range_aux(
+				ForwardIter first,
+				ForwardIter last,
+				numb::forward_iterator_tag)
+		{
+			const size_type n=numb::distance(first,last);
+			this->impl.start=M_allocate(n);//allocate memory 
+			this->impl.end_of_storage=this->impl.start+n;
+			this->impl.finish=std::__uninitialized_copy_a(
+								first,last,this->impl.start,get_Tp_allocator());
+		}
+		/*
+		 * Internal assign functions follow.  The *_aux functions do the actual
+		 * assignment work for the range versions.
+		 * Called by the range assign to implement 
+		 */
+		 //for assign(int,int)
+		 template<typename Integer>
+		 void M_assign_range(Integer n,Integer v,std::true_type)
+		 {	M_fill_assign(n,v);	 }
+		 //for assign(iterator,iterator)
+		 template<typename InputIter>
+		 void M_assign_range(InputIter first,InputIter last,std::false_type)
+		 {	
+			M_assign_range_aux(first,last,
+							   numb::_iterator_category(first));
+         }
+		 //declarations for some other assign function
+		 //they aren't defined as inline functions
+		 template<typename InputIter>
+		 void M_assign_range_aux(InputIter first,InputIter last,
+								 numb::input_iterator_tag);
+		 template<typename ForwardIter>
+		 void M_assign_range_aux(ForwardIter first,ForwardIter last,
+								 numb::forward_iterator_tag);
+		 void M_fill_assign(size_type n,const value_type& v);
+		/*
+		 *Internal insert functions follow.
+		 */
+		 void M_insert_aux(iterator p,const value_type& v);
+		/*
+		 * Internal erase functions follow.
+		 * Called by erase(q1,q2), clear(), resize(), M_fill_assign,
+		 * M_assign_aux.
+		 */
+		void
+		M_erase_at_end(pointer p)
+		{
+			std::_Destroy(p, 
+						this->impl.finish,
+						get_Tp_allocator());
+			this->impl.finish = p;
+		}
 	};//end of vector
+	//public:operator=
+	template<typename Tp,typename Alloc>
+	vector<Tp,Alloc>&
+	vector<Tp,Alloc>::operator=(const vector<Tp,Alloc>& x)
+	{
+		if(&x!=this)//avoid self-assign
+		{
+			const size_type xlen=x.size();
+			if(xlen>capacity())	//need more memory	
+			{
+				//allocate and copy to buffer
+				pointer tmp=M_allocate_and_copy(xlen,x.begin(),x.end());
+				//destroy the all the old items
+				std::_Destroy(this->impl.start, this->impl.finish,
+								get_Tp_allocator()); 
+				//release the old memory
+				M_deallocate(this->impl.start,capacity());
+				//update current vector
+				this->impl.start=tmp;
+				this->impl.end_of_storage=this->impl.start+xlen;
+			}
+			else if(xlen>size())
+			{
+				//the [start,size()) part,use std::copy
+				std::copy(x.impl.start,
+						  x.impl.start+size(),
+						  this->impl.start);
+				//the [size(),xlen) part,use std::__uninitialized_copy_a
+				std::__uninitialized_copy_a(x.impl.start+size(),
+							x.impl.finish,
+							this->impl.finish,
+							get_Tp_allocator());
+			}
+			else
+			{
+				//copy all items of x to *this
+				//destroy the rest 
+				std::_Destroy(
+					std::copy(x.begin(),
+							  x.end(),
+							  begin()),
+					end(),
+					get_Tp_allocator());
+			}
+			this->impl.finish=this->impl.start+xlen;
+		}
+		return *this;
+	}
+	//protected:M_fill_assign(n,v)
+	template<typename Tp,typename Alloc>
+	void
+	vector<Tp,Alloc>::M_fill_assign(size_type n,const value_type& v)
+	{
+		if(n>capacity())//need more memory
+		{
+			vector<Tp,Alloc> tmp(n,v,get_Tp_allocator());
+			tmp.swap(*this);
+			//tmp will be destroyed once out of this scope
+		}
+		else if(n>size())
+		{
+			//For [begin,size) part,use std::fill
+			std::fill(begin(),end(),v);
+			//For [size,n) part,use std::__uninitialized_fill
+			std::__uninitialized_fill_n_a(end(),n-size(),v,
+							get_Tp_allocator());
+			this->impl.finish=this->impl.start+n;
+		}
+		else
+		{
+			//fill [begin,begin+n) with v,erase the rest[begin+n,end)
+			M_erase_at_end(std::fill_n(this->impl.start,n,v));
+		}
+	}
+	//protected:M_assign_range_aux(first,last,iterator_tag)
+	template<typename Tp,typename Alloc> template<typename InputIter> 
+	void vector<Tp,Alloc>::
+	M_assign_range_aux(InputIter first,
+					   InputIter last,
+					   numb::input_iterator_tag)
+	{
+		pointer cur(this->impl.start);
+		for(;cur!=this->impl.finish&&first!=last;++first,++cur)
+				*cur=*first;
+		if(first==last)
+			M_erase_at_end(cur);
+		else
+			insert(end(),first,last);		
+	}
+	template<typename Tp,typename Alloc> template<typename FwdIter> 
+	void vector<Tp,Alloc>::
+	M_assign_range_aux(FwdIter first,
+					   FwdIter last,
+					   numb::forward_iterator_tag)
+	{
+		const size_type len=numb::distance(first,last);
+		if(len>capacity())//need more memory
+		{
+			//allocate and copy to buffer
+			pointer tmp(M_allocate_and_copy(len,first,last));
+			//destroy the old items
+			std::_Destroy(this->impl.start,this->impl.finish,get_Tp_allocator());
+			//release the old memory
+			M_deallocate(this->impl.start,capacity());
+			//update
+			this->impl.start=tmp;
+			this->impl.end_of_storage=this->impl.start+len;
+		}
+		else if(len>size())
+		{
+			//Note:Unlike operator =
+			//Forward iterator doesn't support first+size()
+			FwdIter mid=first;
+			numb::advance(mid,size());	//instead of first+size()
+			//For [begin,end),use std::copy
+			std::copy(first,mid,this->impl.start);
+			//For the rest
+			std::__uninitialized_copy_a(mid,last,this->impl.finish,
+											get_Tp_allocator());				
+		}
+		else
+		{
+			std::_Destroy(
+					std::copy(first,
+							  last,
+							  begin()),
+					end(),
+					get_Tp_allocator());
+		}
+		this->impl.finish=this->impl.start+len;
+	}	
+	//public:insert(p,v)
+	template<typename Tp,typename Alloc>
+	typename vector<Tp,Alloc>::iterator
+    vector<Tp,Alloc>::insert(iterator p, const value_type& v)
+	{
+		const size_type n=p-begin();
+		if(this->impl.finish!=this->impl.end_of_storage
+			&&p==end())		//insert to the back
+		{
+			this->impl.construct(this->impl.finish,v);
+			++this->impl.finish;
+		}
+		else
+		{
+			M_insert_aux(p,v);
+		}
+		return iterator(this->impl.start + n);
+	}
+	//protected:M_insert_aux(p,v)
+	template<typename _Tp, typename _Alloc>
+    void
+    vector<_Tp, _Alloc>::
+    M_insert_aux(iterator p, const value_type& v)
+	{
+		if(this->impl.finish!=this->impl.end_of_storage)
+		{
+			//initialize the item this->impl.finish with last item
+			this->impl.construct(this->impl.finish,*(this->impl.finish-1));
+			++this->impl.finish;
+			value_type v_copy=v;
+			//copy [p,end-2) to [p+1,end-1)
+			std::copy_backward(p.base(),this->impl.finish-2,this->impl.finish-1);	
+			*p=v_copy;	//insert to p;
+		}
+		else
+		{
+			const size_type old_size=size();	//size()==capacity()
+			const size_type new_size=old_size!=0?old_size<<1:1;
+			pointer new_start=M_allocate(new_size);
+			pointer new_finish=new_start;
+			try
+			{
+				//copy [begin,p) to [new_start,new_p)
+				new_finish=std::__uninitialized_copy_a(
+							this->impl.start,p.base(),new_start,get_Tp_allocator());
+				this->impl.construct(new_finish,v); //insert v to p
+				++new_finish;
+				//copy [p+1,end) to [new_p+1,new_finish)
+				new_finish=std::__uninitialized_copy_a(
+							p.base(),this->impl.finish,new_finish,get_Tp_allocator());
+			}
+			catch(...)
+			{
+				M_deallocate(new_start,new_size);
+				throw;
+			}
+			//destory the old items
+			std::_Destroy(this->impl.start,this->impl.finish,get_Tp_allocator());
+			//release memory
+			M_deallocate(this->impl.start,capacity());
+			//update
+			this->impl.start = new_start;
+			this->impl.finish = new_finish;
+			this->impl.end_of_storage = new_start + new_size;
+		}
+	}
+	//to do: M_fill_insert
 	
-	//to do:Vector equality comparisons.
+	//to do: M_erase_at_end
+	//to do: vector equality comparisons.
  }//end of namespace
  #endif
